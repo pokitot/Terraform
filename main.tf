442a2b8ab8c0d36d609e43b78787bfe749be89ea
Pungu Okito <pokito@c02gc1h3md6n.myfiosgateway.com>
2023-04-20 13:44:59 -0400

    commit

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Configure the required providers
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 2.50.2" # Version 2.50.2 or later
+    }
+  }
+}
+
+# Configure the New Relic provider with your account ID and API key
+provider "newrelic" {
+  account_id = var.newrelic_account_id
+  api_key    = var.newrelic_api_key
+}
+
+# Create a New Relic alert policy for monitoring memory usage
+resource "newrelic_alert_policy" "memory_usage" {
+  name = "Memory_usage"
+}
+
+# Create a New Relic NRQL alert condition for memory usage warning
+resource "newrelic_nrql_alert_condition" "memory_warning" {
+  policy_id = newrelic_alert_policy.memory_usage.id
+  name      = "Memory Usage Warning"
+  type      = "static"
+
+  # Define the NRQL query for monitoring average memory used percentage
+  nrql {
+    query             = "SELECT average(memoryUsedPercent) as 'Used Memory' FROM SystemSample WHERE hostname IS NOT NULL"
+    evaluation_offset = "1"
+  }
+
+  value_function       = "single_value"
+  violation_time_limit = "TWENTY_FOUR_HOURS"
+
+  # Define the critical threshold for the alert condition
+  critical {
+    operator              = "above"
+    threshold             = "90"
+    threshold_duration    = "300"
+    threshold_occurrences = "ALL"
+  }
+
+  # Define the warning threshold for the alert condition
+  warning {
+    operator              = "above"
+    threshold             = "80"
+    threshold_duration    = "300"
+    threshold_occurrences = "ALL"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+
+variable "newrelic_account_id" {
+  description = "New Relic account ID"
+  type        = string
+}
+
+variable "newrelic_api_key" {
+  description = "New Relic API key"
+  type        = string
+}
