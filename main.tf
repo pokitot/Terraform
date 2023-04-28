723ee2480fc87fa1d480b52d4a078186d43740f8
Pungu Okito <pokito@c02gc1h3md6n.myfiosgateway.com>
2023-04-28 10:23:41 -0400

    browser alert

--- main.tf
+++ main.tf
@@ -50,3 +50,42 @@ resource "newrelic_nrql_alert_condition" "memory_warning" {
     threshold_occurrences = "ALL"
   }
 }
+resource "newrelic_alert_policy" "password_generator_policy" {
+  name = "PassWord Generator Policy"
+}
+
+resource "newrelic_nrql_alert_condition" "password_generator_warning_condition" {
+  policy_id       = newrelic_alert_policy.password_generator_policy.id
+  name            = "PassWord Generator - Warning"
+  value_function  = "single_value"
+
+  nrql {
+    query           = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    evaluation_offset = "2"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = "0.3"
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
+
+resource "newrelic_nrql_alert_condition" "password_generator_critical_condition" {
+  policy_id       = newrelic_alert_policy.password_generator_policy.id
+  name            = "PassWord Generator - Critical"
+  value_function  = "single_value"
+
+  nrql {
+    query           = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    evaluation_offset = "2"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = "0.3"
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+}
