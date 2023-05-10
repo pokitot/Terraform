1945f8c63ab240e0e9e25beab9780c4d3da1ddb9
Pungu Okito <pokito@c02gc1h3md6n.myfiosgateway.com>
2023-05-10 10:42:59 -0400

    update

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@
 terraform {
   required_providers {
     newrelic = {
-      source  = "newrelic/newrelic"
-      version = "~> 2.50.2" # Version 2.50.2 or later
+      source = "newrelic/newrelic"
+      # Version 2.50.2 or later
+      version = "~> 2.50.2"
     }
   }
 }
@@ -50,42 +51,65 @@ resource "newrelic_nrql_alert_condition" "memory_warning" {
     threshold_occurrences = "ALL"
   }
 }
+# Define a New Relic alert policy named "PassWord Generator Policy"
 resource "newrelic_alert_policy" "password_generator_policy" {
   name = "PassWord Generator Policy"
 }
 
+# Define a NRQL alert condition for the above policy
+# This condition is for warning situations, where average page load duration is above 0.3 for more than 600 seconds
 resource "newrelic_nrql_alert_condition" "password_generator_warning_condition" {
-  policy_id       = newrelic_alert_policy.password_generator_policy.id
-  name            = "PassWord Generator - Warning"
-  value_function  = "single_value"
+  # Reference the ID of the above defined alert policy
+  policy_id = newrelic_alert_policy.password_generator_policy.id
+  name      = "PassWord Generator - Warning"
+  # This alert is based on a single value, not a summary of all data points
+  value_function = "single_value"
 
+  # Define the NRQL query to be used for this alert condition
   nrql {
-    query           = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    # Query average duration from PageView events for 'PassWord Generator' application
+    query = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    # Set the evaluation offset to 2 minutes
     evaluation_offset = "2"
   }
 
+  # Define the critical situation for this condition
   critical {
-    operator              = "above"
-    threshold             = "0.3"
-    threshold_duration    = 600
+    # The critical situation is when the average duration is above 0.3
+    operator  = "above"
+    threshold = "0.3"
+    # The duration for which the threshold has to be crossed for the alert to be triggered is 600 seconds
+    threshold_duration = 600
+    # Alert will be triggered only if all data points in the duration window violate the threshold
     threshold_occurrences = "ALL"
   }
 }
 
+# Define a NRQL alert condition for the above policy
+# This condition is for critical situations, where average page load duration is above 0.3 for more than 300 seconds
 resource "newrelic_nrql_alert_condition" "password_generator_critical_condition" {
-  policy_id       = newrelic_alert_policy.password_generator_policy.id
-  name            = "PassWord Generator - Critical"
-  value_function  = "single_value"
+  # Reference the ID of the above defined alert policy
+  policy_id = newrelic_alert_policy.password_generator_policy.id
+  name      = "PassWord Generator - Critical"
+  # This alert is based on a single value, not a summary of all data points
+  value_function = "single_value"
 
+  # Define the NRQL query to be used for this alert condition
   nrql {
-    query           = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    # Query average duration from PageView events for 'PassWord Generator' application
+    query = "SELECT average(duration) FROM PageView WHERE appName = 'PassWord Generator'"
+    # Set the evaluation offset to 2 minutes
     evaluation_offset = "2"
   }
 
+  # Define the critical situation for this condition
   critical {
-    operator              = "above"
-    threshold             = "0.3"
-    threshold_duration    = 300
+    # The critical situation is when the average duration is above 0.3
+    operator  = "above"
+    threshold = "0.3"
+    # The duration for which the threshold has to be crossed for the alert to be triggered is 300 seconds
+    threshold_duration = 300
+    # Alert will be triggered only if all data points in the duration window violate the threshold
     threshold_occurrences = "ALL"
   }
 }
